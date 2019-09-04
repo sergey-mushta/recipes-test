f392b406b47be20f7159b0a1c2744ad11b4dbc7e
Sergey M <romgr@ex-crypto.me>
2019-09-04 14:34:44 +0300

    minor fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,25 +4,29 @@ import {Link} from "react-router-dom";
 
 export default class Header extends Component {
 
+/*
+<ul className="navbar-nav mr-auto">
+<li className="nav-item active">
+<a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+</li>
+    */
     render() {
         return  (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
             <div className="container">
-                <div className="col-md-3">
-                    <Link to="/">Recipes backend</Link>
-                </div>
-                <div className="col-md-9">
-                    <ul>
-                        <li>
-                            <Link to="/manage_categories">Categories</Link>
+                <div className="col-md-12">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Home</Link>
                         </li>
-                        <li>
-                            <Link to="/manage_recipes">Recipes</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/manage_categories">Categories</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/manage_recipes">Recipes</Link>
                         </li>
                     </ul>
                 </div>
-                
-                
             </div>
         </nav>
         );
