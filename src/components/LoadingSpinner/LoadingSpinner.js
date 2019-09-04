296c7353271ef82d76c2743901d0df06af2d3179
Sergey Mushta <futukrishna@gmail.com>
2019-09-04 23:10:57 +0300

    dev

---
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import "./LoadingSpinner.css";
+
+export default class LoadingSpinner extends Component {
+    render() {
+        return  (
+            <div className="loader">Loading...</div>
+        );
+    }
+  
+}
---
+++ src/redux/actions/index.js
@@ -0,0 +1,4 @@
+export const getCategories = () => ({
+    type: 'GET_CATEGORIES',
+});
+
