d020b1f488da41fc2b6b79345047d1bb873ca543
Sergey M <romgr@ex-crypto.me>
2019-09-06 18:33:24 +0300

    tbl dev

--- src/components/ManageCategories/ManageCategories.js
+++ src/components/ManageCategories/ManageCategories.js
@@ -10,11 +10,42 @@ class ManageCategories extends Component {
     }
 
     render() {
+
+        const items = this.props.categories !== undefined ? this.props.categories.map((item) => {
+            return (
+                <tr key={item._id}>
+                    <th scope="row">{item.title}</th>
+                    <td>{item.parentId}</td>
+                    <td>{item.updatedAt}</td>
+                    <td>
+                        <a className="btn btn-secondary mr-3" href="#">
+                            <i className="fa fa-pencil"></i></a>
+                        <a className="btn btn-danger mr-3" href="#">
+                            <i className="fa fa-trash"></i></a>
+                    </td>
+                </tr>
+            );
+        }) : '';
+
         return  (
             <span>
-                <h3>Manage Categories</h3>
+                <h3 className="mb-3">Manage Categories</h3>
                 <LoadingSpinner />
-                <div>{JSON.stringify(this.props.categories)}</div>
+                <a className="btn btn-secondary mb-3 mt-3" href="#">
+                    <i className="fa fa-plus"></i></a>
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Name</th>
+                      <th scope="col">Parent ID</th>
+                      <th scope="col">Updated at</th>
+                      <th scope="col"></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {items}
+                  </tbody>
+                </table>
             </span>
         );
     }
