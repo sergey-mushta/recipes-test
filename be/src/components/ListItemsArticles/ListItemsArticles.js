c36f5d211086f38eafae8af41db24fc01a62d8d0
Sergey M <romgr@ex-crypto.me>
2019-09-13 17:14:47 +0300

    backend moved

--- be/src/components/ListItemsArticles/ListItemsArticles.js
+++ be/src/components/ListItemsArticles/ListItemsArticles.js
@@ -2,73 +2,72 @@ import React, { Component } from 'react';
 import Button from "react-bootstrap/Button";
 import {connect} from "react-redux";
 import FormElements from "../FormElements"
-import {EDIT_RECIPE_FORM} from "../../forms";
+import {EDIT_ARTICLE_FORM } from "../../forms";
 import { prepareCategoriesForSelect } from "../../services";
-import {deleteRecipe, editRecipe, initModal} from "../../redux/actions";
+import {initModal, deleteArticle, editArticle} from "../../redux/actions";
 
-class ListItemsRecipes extends Component {
+class ListItemsArticles extends Component {
     static defaultProps = { parentId: null }
 
-    initEditModal(recipeId) {
+    initEditModal(articleId) {
         this.props.initModal({
-            titleText: 'Edit recipe',
+            titleText: 'Edit article',
             isForm: true,
-            bodyText: <FormElements {...EDIT_RECIPE_FORM} values={ this.props.recipes.find((e) => e['_id'] === recipeId) } selectOptions={{ categoryId: prepareCategoriesForSelect(this.props.categories) }}/>,
+            bodyText: <FormElements {...EDIT_ARTICLE_FORM} values={ this.props.articles.find((e) => e['_id'] === articleId) } selectOptions={{ categoryId: prepareCategoriesForSelect(this.props.categories) }}/>,
             closeBtnText: 'Cancel',
             confirmBtnText: 'Edit',
-            closeBtnVariant: 'secondary',
             confirmBtnVariant: 'success',
-            confirmAddData: { type: 'EDIT_RECIPE', '_id': recipeId  },
+            confirmAddData: { type: 'EDIT_ARTICLE', '_id': articleId  },
             closeHandler: (props) => { props.hideModal(); },
-            formSubmitHandler: (e, props) => { this.props.editRecipe(props.currentFormValues); e.preventDefault(); }
+            formSubmitHandler: (e, props) => { this.props.editArticle(props.currentFormValues); e.preventDefault(); }
         });
     }
 
-    initDeleteModal(recipeId) {
+    initDeleteModal(articleId) {
         this.props.initModal({
-            titleText: 'Delete recipe',
+            titleText: 'Delete article',
             bodyText: 'Are you sure?',
             closeBtnText: 'Cancel',
             confirmBtnText: 'Delete',
-            closeBtnVariant: 'secondary',
-            confirmBtnVariant: 'danger',
-            confirmAddData: { type: 'DELETE_RECIPE', '_id': recipeId },
+            confirmAddData: { type: 'DELETE_ARTICLE', '_id': articleId },
             closeHandler: (props) => { props.hideModal(); },
-            confirmHandler: (props) => { this.props.deleteRecipe(props.confirmAddData._id); },
+            confirmHandler: (props) => { this.props.deleteArticle(props.confirmAddData._id); },
         });
     }
 
-    initViewModal(recipeId) {
-        const item = this.props.recipes.find((e) => e['_id'] === recipeId);
+    initViewModal(articleId) {
+        const item = this.props.articles.find((e) => e['_id'] === articleId);
         this.props.initModal({
-            titleText: 'View recipe',
+            titleText: 'View article',
             bodyText: (<>
-                            <p><b>Category:</b> {(this.props.categories.find((e) => e._id === item.categoryId) !== undefined ) ? this.props.categories.find((e) => e._id === item.categoryId).title : 'NOT FOUND'}</p>
-                            <p><b>Title:</b> { item.title }</p>
-                            <p><b>Text:</b> { item.text }</p>
-                        </>),
+                <p><b>Category:</b> {(this.props.categories.find((e) => e._id === item.categoryId) !== undefined ) ? this.props.categories.find((e) => e._id === item.categoryId).title : 'NOT FOUND'}</p>
+                <p><b>Title:</b> { item.title }</p>
+                <p><b>Description:</b> { item.title }</p>
+                <p><b>Text:</b> { item.text }</p>
+            </>),
             closeBtnText: 'Close',
             confirmBtnText: '',
             closeBtnVariant: 'secondary',
             closeHandler: (props) => { props.hideModal(); },
         });
     }
-
     render() {
         return <table className="table">
             <thead>
-                <tr>
-                    <th scope="col">Category</th>
-                    <th scope="col">Title</th>
-                    <th scope="col"></th>
-                </tr>
+            <tr>
+                <th scope="col">Category</th>
+                <th scope="col">Title</th>
+                <th scope="col">Description</th>
+                <th scope="col"></th>
+            </tr>
             </thead>
             <tbody>
-            {this.props.recipes.sort((a, b) => a.title.localeCompare(b.title)).map((item) => {
+            {this.props.articles.sort((a, b) => a.title.localeCompare(b.title)).map((item) => {
                 return (
                     <tr key={item._id}>
                         <td>{(this.props.categories.find((e) => e._id === item.categoryId) !== undefined ) ? this.props.categories.find((e) => e._id === item.categoryId).title : 'NOT FOUND'}</td>
                         <td>{item.title}</td>
+                        <td>{item.description}</td>
                         <td className="pl-2">
                             <Button variant="secondary" size="sm" className="m-1" onClick={() => { this.initViewModal(item._id) } }><i className="fa fa-eye"></i></Button>
                             <Button variant="secondary" size="sm" className="m-1" onClick={() => { this.initEditModal(item._id) } }><i className="fa fa-pencil"></i></Button>
@@ -83,4 +82,4 @@ class ListItemsRecipes extends Component {
 
 }
 
-export default connect((state) => ({ categories: state.categories, recipes: state.recipes }), {initModal, deleteRecipe, editRecipe })(ListItemsRecipes);
+export default connect((state) => ({ categories: state.categories, articles: state.articles }), {initModal, deleteArticle, editArticle })(ListItemsArticles);
--- be/src/components/ListItemsRecipes/ListItemsRecipes.js
+++ be/src/components/ListItemsRecipes/ListItemsRecipes.js
@@ -2,75 +2,70 @@ import React, { Component } from 'react';
 import Button from "react-bootstrap/Button";
 import {connect} from "react-redux";
 import FormElements from "../FormElements"
-import {EDIT_ARTICLE_FORM } from "../../forms";
+import {EDIT_RECIPE_FORM} from "../../forms";
 import { prepareCategoriesForSelect } from "../../services";
-import {initModal, deleteArticle, editArticle} from "../../redux/actions";
+import {deleteRecipe, editRecipe, initModal} from "../../redux/actions";
 
-class ListItemsArticles extends Component {
+class ListItemsRecipes extends Component {
     static defaultProps = { parentId: null }
 
-    initEditModal(articleId) {
+    initEditModal(recipeId) {
         this.props.initModal({
-            titleText: 'Edit article',
+            titleText: 'Edit recipe',
             isForm: true,
-            bodyText: <FormElements {...EDIT_ARTICLE_FORM} values={ this.props.articles.find((e) => e['_id'] === articleId) } selectOptions={{ categoryId: prepareCategoriesForSelect(this.props.categories) }}/>,
+            bodyText: <FormElements {...EDIT_RECIPE_FORM} values={ this.props.recipes.find((e) => e['_id'] === recipeId) } selectOptions={{ categoryId: prepareCategoriesForSelect(this.props.categories) }}/>,
             closeBtnText: 'Cancel',
             confirmBtnText: 'Edit',
-            closeBtnVariant: 'secondary',
             confirmBtnVariant: 'success',
-            confirmAddData: { type: 'EDIT_ARTICLE', '_id': articleId  },
+            confirmAddData: { type: 'EDIT_RECIPE', '_id': recipeId  },
             closeHandler: (props) => { props.hideModal(); },
-            formSubmitHandler: (e, props) => { this.props.editArticle(props.currentFormValues); e.preventDefault(); }
+            formSubmitHandler: (e, props) => { this.props.editRecipe(props.currentFormValues); e.preventDefault(); }
         });
     }
 
-    initDeleteModal(articleId) {
+    initDeleteModal(recipeId) {
         this.props.initModal({
-            titleText: 'Delete article',
+            titleText: 'Delete recipe',
             bodyText: 'Are you sure?',
             closeBtnText: 'Cancel',
             confirmBtnText: 'Delete',
-            closeBtnVariant: 'secondary',
-            confirmBtnVariant: 'danger',
-            confirmAddData: { type: 'DELETE_ARTICLE', '_id': articleId },
+            confirmAddData: { type: 'DELETE_RECIPE', '_id': recipeId },
             closeHandler: (props) => { props.hideModal(); },
-            confirmHandler: (props) => { this.props.deleteArticle(props.confirmAddData._id); },
+            confirmHandler: (props) => { this.props.deleteRecipe(props.confirmAddData._id); },
         });
     }
 
-    initViewModal(articleId) {
-        const item = this.props.articles.find((e) => e['_id'] === articleId);
+    initViewModal(recipeId) {
+        const item = this.props.recipes.find((e) => e['_id'] === recipeId);
         this.props.initModal({
-            titleText: 'View article',
+            titleText: 'View recipe',
             bodyText: (<>
-                <p><b>Category:</b> {(this.props.categories.find((e) => e._id === item.categoryId) !== undefined ) ? this.props.categories.find((e) => e._id === item.categoryId).title : 'NOT FOUND'}</p>
-                <p><b>Title:</b> { item.title }</p>
-                <p><b>Description:</b> { item.title }</p>
-                <p><b>Text:</b> { item.text }</p>
-            </>),
+                            <p><b>Category:</b> {(this.props.categories.find((e) => e._id === item.categoryId) !== undefined ) ? this.props.categories.find((e) => e._id === item.categoryId).title : 'NOT FOUND'}</p>
+                            <p><b>Title:</b> { item.title }</p>
+                            <p><b>Text:</b> { item.text }</p>
+                        </>),
             closeBtnText: 'Close',
             confirmBtnText: '',
             closeBtnVariant: 'secondary',
             closeHandler: (props) => { props.hideModal(); },
         });
     }
+
     render() {
         return <table className="table">
             <thead>
-            <tr>
-                <th scope="col">Category</th>
-                <th scope="col">Title</th>
-                <th scope="col">Description</th>
-                <th scope="col"></th>
-            </tr>
+                <tr>
+                    <th scope="col">Category</th>
+                    <th scope="col">Title</th>
+                    <th scope="col"></th>
+                </tr>
             </thead>
             <tbody>
-            {this.props.articles.sort((a, b) => a.title.localeCompare(b.title)).map((item) => {
+            {this.props.recipes.sort((a, b) => a.title.localeCompare(b.title)).map((item) => {
                 return (
                     <tr key={item._id}>
                         <td>{(this.props.categories.find((e) => e._id === item.categoryId) !== undefined ) ? this.props.categories.find((e) => e._id === item.categoryId).title : 'NOT FOUND'}</td>
                         <td>{item.title}</td>
-                        <td>{item.description}</td>
                         <td className="pl-2">
                             <Button variant="secondary" size="sm" className="m-1" onClick={() => { this.initViewModal(item._id) } }><i className="fa fa-eye"></i></Button>
                             <Button variant="secondary" size="sm" className="m-1" onClick={() => { this.initEditModal(item._id) } }><i className="fa fa-pencil"></i></Button>
@@ -85,4 +80,4 @@ class ListItemsArticles extends Component {
 
 }
 
-export default connect((state) => ({ categories: state.categories, articles: state.articles }), {initModal, deleteArticle, editArticle })(ListItemsArticles);
+export default connect((state) => ({ categories: state.categories, recipes: state.recipes }), {initModal, deleteRecipe, editRecipe })(ListItemsRecipes);
