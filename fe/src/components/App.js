f07b23782ac394ab28e4d6fda37c8d773c5acc7d
Sergey M <romgr@ex-crypto.me>
2019-09-13 18:20:18 +0300

    fe dev

--- fe/src/components/App.js
+++ fe/src/components/App.js
@@ -5,19 +5,37 @@ import Header from "./Header";
 
 import ErrorGlobal from "./ErrorGlobal";
 import {connect} from "react-redux";
+import {getAllCategories} from "../redux/actions";
+import CategoriesTree from "./CategoriesTree/CategoriesTree";
 
 class App extends Component {
+
+    componentDidMount() {
+        this.props.getAllCategories();
+    }
+
     render() {
         return (
             <Router>
                 <Header/>
                 {this.props.errorGlobal && <ErrorGlobal />}
-                <div className="container mt-3">
-                    <Route path="/" render={() => <h3>Welcome to recipes frontend</h3>} exact/>
+                <div className="container mt-3 h-100">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="border border-secondary bg-light p-3 h-100">
+                                <CategoriesTree />
+                            </div>
+                        </div>
+                        <div className="col-md-8">
+                            <div className="border border-secondary bg-light p-3 ml-1 h-100">
+                                <Route path="/" render={() => <h3>Welcome to recipes frontend</h3>} exact/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </Router>
         );
     }
 }
 
-export default connect((state) => ({ errorGlobal: state.errorGlobal}),null)(App)
+export default connect((state) => ({ errorGlobal: state.errorGlobal}),{getAllCategories})(App)
---
+++ fe/src/components/CategoriesTree/CategoriesTree.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {connect} from "react-redux";
+import "./CategoriesTree.css";
+import {NavLink} from "react-router-dom";
+
+class CategoriesTree extends Component {
+    static defaultProps = { parentId: null }
+
+    render() {
+        return this.props.categories !== undefined && (
+            <>
+                {this.props.parentId === null && <h4 className="mb-3">Categories</h4>}
+                <ul className="categories-tree pl-2">
+                    {this.props.categories.filter((e) => e['parentId'] === this.props.parentId).sort((a, b) => a.title.localeCompare(b.title)).map((item) => {
+                        return (
+                            <li key={item._id} >
+                                <NavLink to={"/category/" + item._id}>{item.title}</NavLink>
+                                <CategoriesTree key={'sub_'+item._id} parentId={item._id} categories={this.props.categories}  />
+                            </li>
+                        );
+                    })}
+                </ul>
+            </>
+        );
+    }
+
+}
+
+export default connect((state) => ({ categories: state.categories }), null)(CategoriesTree);
